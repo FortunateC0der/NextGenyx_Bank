1810d04102bc2dd69638aed4ef0b8a5a2fac1c95
FortunateC0der <fortunate.mothoana@gmail.com>
2026-02-20 10:51:12 +0200

    remove component for upgrade

--- force-app/main/default/lwc/footerComponent/footerComponent.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import LOGO from '@salesforce/resourceUrl/NextGenyxBank_Logo';
-
-export default class FooterComponent extends LightningElement {
-    logoUrl = LOGO;
-}

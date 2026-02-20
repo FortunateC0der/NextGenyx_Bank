bbd17ca1fe4c56a24077aea7d3e49c35992edcb7
FortunateC0der <fortunate.mothoana@gmail.com>
2026-02-20 10:56:19 +0200

    updated footer component

---
+++ force-app/main/default/lwc/footerComponent/footerComponent.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import LOGO from '@salesforce/resourceUrl/NexGenyxBank_Logo';
+
+export default class FooterComponent extends LightningElement {
+    logoUrl = LOGO;
+}
